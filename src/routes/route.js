9105ae9790ced05ee22c1e4415abb3cc38869c2c
prahlad <prahladkumar7360@gmail.com>
2022-10-20 16:48:23 +0530

    df

--- src/routes/route.js
+++ src/routes/route.js
@@ -13,6 +13,7 @@ router.get('/test-me', function (req, res) {
     res.send('My first ever api!')
 });
 
+<<<<<<< Updated upstream
 router.get("/movies/:indexNumber", function(req, res){
     const movies = ["Rang de basanti", "The shining", "Lord of the rings", "Batman begins"]
     console.log(req.params.indexNumber)
@@ -21,6 +22,109 @@ router.get("/movies/:indexNumber", function(req, res){
     if(movieIndex<0 || movieIndex>=movies.length) {
         //if the index is invalid send an error message
         return res.send('The index value is not correct, Please check the it')
+=======
+router.get("/random" , function(req, res) {
+    res.send("hi there")
+})
+
+
+router.get("/test-api" , function(req, res) {
+    res.send("hi FunctionUp")
+})
+
+
+router.get("/test-api-2" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API")
+})
+
+
+router.get("/test-api-3" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's ")
+})
+
+
+router.get("/test-api-4" , function(req, res) {
+    res.send("hi FunctionUp. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+
+
+router.get("/test-api-5" , function(req, res) {
+    res.send("hi FunctionUp5. This is another cool API. And NOw i am bored of creating API's. PLZ STOP CREATING MORE API;s ")
+})
+
+router.get("/test-api-6" , function(req, res) {
+    res.send({a:56, b: 45})
+})
+
+router.post("/test-post", function(req, res) {
+    res.send([ 23, 45 , 6])
+})
+
+
+router.post("/test-post-2", function(req, res) {
+    res.send(  { msg: "hi" , status: true }  )
+})
+
+router.post("/test-post-3", function(req, res) {
+    // let id = req.body.user
+    // let pwd= req.body.password
+
+    // console.log( id , pwd)
+
+    console.log( req.body )
+
+    res.send(  { msg: "hi" , status: true }  )
+})
+
+
+
+router.post("/test-post-4", function(req, res) {
+    let arr= [ 12, "functionup"]
+    let ele= req.body.element
+    arr.push(ele)
+    res.send(  { msg: arr , status: true }  )
+})
+
+let players =
+    [
+        {
+            name: "manish",
+            dob: "1/1/1995",
+            gender: "male",
+            city: "jalandhar",
+            sports: [
+                "swimming"
+            ]
+        },
+        {
+            name: "gopal",
+            dob: "1/09/1995",
+            gender: "male",
+            city: "delhi",
+            sports: [
+                "soccer"
+            ]
+        },
+        {
+            name: "lokesh",
+            dob: "1/1/1990",
+            gender: "male",
+            city: "mumbai",
+            sports: [
+                "soccer"
+            ]
+        },
+    ]
+
+router.post("/players-list",function (req, res) {
+    let Allplayerlist = req.body.element
+    for (let i = 0; i<players.length; i++) {
+        if (i.name === Allplayerlist.name) {
+            return res.send("name is Already exists So You can Check");
+            
+        }
+>>>>>>> Stashed changes
     }
 
     //if the index was valid send the movie at that index in response
@@ -28,6 +132,8 @@ router.get("/movies/:indexNumber", function(req, res){
     res.send(requiredMovie)
 })
 
+<<<<<<< Updated upstream
+<<<<<<< Updated upstream
 router.get("/shoes", function(req, res){
     let queryParams = req.query
     let brand = queryParams.brand
@@ -110,3 +216,11 @@ router.get("/films/:filmId", function(req, res){
 
 module.exports = router;
 // adding this comment for no reason
+=======
+
+module.exports = router;
+>>>>>>> Stashed changes
+=======
+
+module.exports = router;
+>>>>>>> Stashed changes
